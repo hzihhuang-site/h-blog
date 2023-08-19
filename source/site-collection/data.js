cfb7472635ace53825ad70d28605f4265916b87d
v_hzihhuang <v_hzihhuang@tencent.com>
2023-08-19 15:56:08 +0800

    perf: 优化

---
+++ source/site-collection/data.js
@@ -0,0 +1,87 @@
+export const frameworks = [
+  {
+    href: 'https://cn.vuejs.org/',
+    svg: '<svg viewBox="0 0 128 128" width="32" height="32" data-v-c0161dce=""><path fill="#42b883" d="M78.8,10L64,35.4L49.2,10H0l64,110l64-110C128,10,78.8,10,78.8,10z" data-v-c0161dce=""></path><path fill="#35495e" d="M78.8,10L64,35.4L49.2,10H25.6L64,76l38.4-66H78.8z" data-v-c0161dce=""></path></svg>',
+    text: 'Vue',
+  },
+  {
+    href: 'https://react.docschina.org/',
+    svg: '<svg width="32" height="32" viewBox="-10.5 -9.45 21 18.9" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="0" cy="0" r="2" fill="#85d8f3"></circle> <g stroke="#85d8f3" stroke-width="1" fill="none"> <ellipse rx="10" ry="4.5"></ellipse><ellipse rx="10" ry="4.5" transform="rotate(60)"></ellipse> <ellipse rx="10" ry="4.5" transform="rotate(120)"></ellipse> </g> </svg>',
+    text: 'React'
+  },
+  {
+    href: 'https://angular.cn/',
+    url: 'https://angular.cn/assets/images/logos/angular/angular.svg',
+    urlStyle: { marginRight: '-8px' },
+    text: 'ngular',
+  },
+  {
+    href: 'https://docs.nestjs.cn/',
+    url: 'https://d33wubrfki0l68.cloudfront.net/e937e774cbbe23635999615ad5d7732decad182a/26072/logo-small.ede75a6b.svg',
+    text: 'Nestjs',
+  },
+  {
+    href: 'https://taro.jd.com/',
+    url: '/blog/assets/images/slit-collection/taro-logo.png',
+    text: 'Taro',
+  },
+  {
+    href: 'https://www.dcloud.io/',
+    url: 'https://web-assets.dcloud.net.cn/unidoc/zh/logo.png',
+    urlStyle: { width: 'auto', height: '60%', aspectRatio: '160 / 56' },
+  },
+]
+
+export const utils = [
+  {
+    href: 'https://picular.co/',
+    text: 'Picular',
+    desc: '根据你搜索的物品返回物品对应的颜色'
+  },
+  {
+    href: 'https://colorhunt.co/',
+    text: 'ColorHunt',
+    desc: '相近色搭配'
+  },
+  {
+    href: 'https://colordrop.io/',
+    text: 'ColorDrop',
+    desc: '相近色搭配'
+  },
+  {
+    href: 'https://color.oulu.me/',
+    text: '渐变颜色',
+    desc: '背景渐变色集合',
+  },
+  {
+    href: 'https://matthewlein.com/tools/ceaser',
+    text: '贝塞尔曲线',
+    desc: '图形化生成贝塞尔曲线'
+  },
+  {
+    href: 'https://www.toptal.com/developers/css/sprite-generator',
+    url: 'https://www.toptal.com/developers/css/sprite-generator/static/images/logo_header.svg',
+    urlStyle: { width: 'auto', height: '54%', aspectRatio: '160 / 56' },
+    desc: '序列帧转精灵图'
+  },
+  {
+    href: 'https://unbug.github.io/codelf/',
+    text: 'CODELF',
+    desc: '变量命名'
+  },
+  {
+    href: 'https://www.json.cn/',
+    text: 'Json.cn',
+    desc: 'json 解析和格式化'
+  },
+  {
+    href: 'https://wangwl.net/static/projects/visualRegex',
+    text: 'visualRegex',
+    desc: '正则可视化'
+  },
+  {
+    href: 'https://pagespeed.web.dev/',
+    text: 'pagespeed',
+    desc: '在线 Web 网站性能评测'
+  }
+]
